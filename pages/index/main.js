2669b12d9fdddbcab1434bc614c01345432157e7
zhangheng@allinmd.cn <zhangheng123>
2018-02-02 18:03:01 +0800

    项目：新年祝福小程序实现 描述：弹层添加，loading实现，popou添加 reviewer：firefly

--- pages/index/main.js
+++ pages/index/main.js
@@ -1,5 +1,30 @@
 Page({
     onReady:function(){
      console.log("加载出来");
+
+    },
+    data: {
+        inputName: '',
+        loading:false,
+        errorDes:true,
+        errorStr:""
+    },
+    popup:function(txt){
+        this.setData({
+            errorStr: txt
+        })
+    },
+    nameInput:function(e){
+        this.setData({
+            inputName: e.detail.value
+        })
+    },
+    createMyDes:function(){
+        var t = this;
+        if(t.data.inputName.length){
+            t.popup("请输入您的名字");
+        }else{
+
+        }
     }
 });
